1096863635758d0f6792ea64c4a3828b8d12678b
Federico Rulli <fede.rulli@hotmail.com>
2017-10-31 19:33:50 -0300

    mas tests

--- src/test/java/ar/uba/fi/tdd/rulogic/model/InterpreterTests.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/InterpreterTests.java
@@ -8,30 +8,44 @@ import ar.uba.fi.tdd.rulogic.Interpreter;
 public class InterpreterTests {
 	
 	@Test
-	public void query_should_be_true1() throws Exception{
+	public void varon_juan_true() throws Exception{
 		Interpreter interpreter = new Interpreter();
 		interpreter.parse_db("src/main/resources/rules.db");
 		Assert.assertTrue(interpreter.check_query("varon(juan)"));
 	}
 	
 	@Test
-	public void query_should_be_true2() throws Exception{
+	public void mujer_maria_true() throws Exception{
 		Interpreter interpreter = new Interpreter();
 		interpreter.parse_db("src/main/resources/rules.db");
 		Assert.assertTrue(interpreter.check_query("mujer(maria) ."));
 	}
 	
 	@Test
-	public void query_should_be_false1() throws Exception{
+	public void mujer_mario_false() throws Exception{
 		Interpreter interpreter = new Interpreter();
 		interpreter.parse_db("src/main/resources/rules.db");
 		Assert.assertFalse(interpreter.check_query("mujer(mario) ."));
 	}
 	
 	@Test
-	public void query_should_be_true3() throws Exception{
+	public void hijo_pepe_juan_true() throws Exception{
 		Interpreter interpreter = new Interpreter();
 		interpreter.parse_db("src/main/resources/rules.db");
 		Assert.assertTrue(interpreter.check_query("hijo(pepe, juan)."));
+	}
+	
+	@Test
+	public void hija_maria_roberto_false() throws Exception{
+		Interpreter interpreter = new Interpreter();
+		interpreter.parse_db("src/main/resources/rules.db");
+		Assert.assertFalse(interpreter.check_query("hija(maria, roberto)."));
+	}
+	
+	@Test
+	public void tio_nicolas_alejandro_roberto_true() throws Exception{
+		Interpreter interpreter = new Interpreter();
+		interpreter.parse_db("src/main/resources/rules.db");
+		Assert.assertTrue(interpreter.check_query("tio(nicolas,alejandro, roberto)."));
 	}
 }
